d2cd5f439c5f0375b809c6ebe338564b3c19040a
shahrokh <shahrokhelmi@gmail.com>
2022-01-21 16:32:31 +0330

    add classes

--- script.js
+++ script.js
@@ -11,6 +11,64 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+/*========================================================================================
+                                         WORKOUT
+==========================================================================================*/
+class Workout {
+  #id = Date.now();
+  #date = months[new Date().getMonth()] + " " + new Date().getDate();
+
+  constructor(distance, duration, coords) {
+    this.distance = distance;
+    this.duration = duration;
+    this.coords = coords;
+  }
+
+  _setDescription() {
+    this.description = this.type + " on " + this.#date;
+  }
+}
+
+/*========================================================================================
+                                         RUNNING
+==========================================================================================*/
+class Running extends Workout {
+  #pace;
+  type = "running";
+  constructor(distance, duration, coords, cadence) {
+    super(distance, duration, coords);
+    this.cadence = cadence;
+    this._setDescription();
+  }
+
+  #calcPace() {
+    this.#pace = this.duration / this.distance;
+  }
+}
+
+/*========================================================================================
+                                         CYCLING
+==========================================================================================*/
+class Cycling extends Workout {
+  #speed;
+  type = "cycling";
+  constructor(distance, duration, coords, elevationGain) {
+    super(distance, duration, coords);
+    this.elevationGain = elevationGain;
+    this._setDescription();
+  }
+
+  calcSpeed() {
+    this.#speed = this.duration.distance;
+  }
+}
+
+const c1 = new Cycling(1, 1, 1, 1);
+const r1 = new Running(1, 1, 1, 1);
+
+/*========================================================================================
+                                           App
+==========================================================================================*/
 class App {
   #map;
   #mapEvent;
@@ -22,12 +80,14 @@ class App {
   }
 
   #getPosition() {
-    navigator.geolocation.getCurrentPosition(
-      this.#loadMap.bind(this),
-      function () {
-        console.log("Could not get you Location");
-      }
-    );
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this.#loadMap.bind(this),
+        function () {
+          console.log("Could not get you Location");
+        }
+      );
+    }
   }
 
   #loadMap(position) {
@@ -58,8 +118,44 @@ class App {
 
   #newWorkout(e) {
     e.preventDefault();
-
     const { lat, lng } = this.#mapEvent.latlng;
+
+    const allAreNumber = (...inputs) =>
+      inputs.every((input) => isFinite(input));
+    const allArePositive = (...inputs) => inputs.every((input) => input > 0);
+
+    const type = inputType.value;
+    let workout;
+    if (type === "running") {
+      const distance = +inputDistance.value;
+      const duration = +inputDuration.value;
+      const cadence = +inputCadence.value;
+
+      // guard cond.
+      if (
+        !allAreNumber(distance, duration, cadence) ||
+        !allArePositive(distance, duration, cadence)
+      )
+        return alert("Something Wrong Master!");
+
+      workout = new Running(distance, duration, [lat, lng], cadence);
+    }
+
+    if (type === "cycling") {
+      const distance = +inputDistance.value;
+      const duration = +inputDuration.value;
+      const elevationGain = +inputElevation.value;
+
+      // guard cond.
+      if (
+        !allAreNumber(distance, duration, elevationGain) ||
+        !allArePositive(distance, duration)
+      )
+        return alert("Something Wrong Master!");
+
+      workout = new Cycling(duration, duration, [lat, lng], elevationGain);
+    }
+
     L.marker([lat, lng])
       .addTo(this.#map)
       .bindPopup(
@@ -68,11 +164,17 @@ class App {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: "running-popup",
+          className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent("workout")
+      .setPopupContent(workout.description)
       .openPopup();
+
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        "";
   }
 }
 
